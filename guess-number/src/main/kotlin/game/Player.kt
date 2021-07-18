dacccd1d0ef27d2d8dc2e53d6cf9cc077c54b6dc
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-07-18 15:21:19 +0200

    dev: avoid null case

--- guess-number/src/main/kotlin/game/Player.kt
+++ guess-number/src/main/kotlin/game/Player.kt
@@ -1,16 +1,16 @@
 package game
 
-data class Player(private var name: String? = null) {
+data class Player(private var name: String = "") {
 
     init {
-        if (name == null) {
+        if (name == "") {
             println("Hello what is your name?")
             name = readLine() ?: throw Error("bad input")
         }
     }
 
     fun getName(): String {
-        return name ?: throw Error("should not come here");
+        return name
     }
 
 }
